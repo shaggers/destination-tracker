d0505f86d4522c1af149233fe53594edd85f1141
shaggers <camtl66@gmail.com>
2019-04-22 08:52:15 -0500

    Scaffolding complete

---
+++ src/config/main-config.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const logger = require('morgan');
+
+module.exports = {
+    init(app, express){
+        app.set("views", viewsFolder);
+        app.set("view engine", "ejs");
+        app.use(express.static(path.join(__dirname, "..", "assets")));
+        app.use(logger('dev'));
+    }
+};
